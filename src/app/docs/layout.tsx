291cdd8d48471a586b49a13577fa407b4406820b
Haitao Pan <haitaopanhq@gmail.com>
2026-08-06 20:08:15 +0800

    fix: replace UnifiedNavigation with MarketingNav in docs

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,5 +1,5 @@
 import DocsSidebar from "./DocsSidebar";
-import UnifiedNavigation from "@components/UnifiedNavigation";
+import MarketingNav from "@/components/marketing/MarketingNav";
 import Footer from "@components/Footer";
 
 export default function DocsLayout({
@@ -9,7 +9,7 @@ export default function DocsLayout({
 }) {
   return (
     <div className="flex min-h-screen flex-col bg-background text-text">
-      <UnifiedNavigation />
+      <MarketingNav />
       <div className="flex w-full flex-1 items-start px-2 pb-8 pt-3 sm:px-3 lg:px-4">
         <DocsSidebar />
         <main className="min-h-[calc(100vh-64px)] flex-1 overflow-x-hidden px-3 py-3 sm:px-5 lg:px-6">
